2eaf046138fdc3f4fe051bb9ea14d41eca05ef01
hdpuk86 <hayley.d.prior@gmail.com>
2021-10-24 20:50:09 +0100

    create a navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def main_nav_class(page_url)
+    return 'nav-link active' if current_page?(page_url)
+
+    'nav-link'
+  end
+
+  def main_nav_aria(page_url)
+    return 'page' if current_page?(page_url)
+
+    false
+  end
 end
